38964887789f15330612413f14b1184c1d0540c3
Daniel Almeida <danielf.almeida@gmail.com>
2019-07-05 22:49:42 +0100

    Simple Hello World example with ui tests

--- HelloWorld/AppDelegate.swift
+++ HelloWorld/AppDelegate.swift
@@ -12,7 +12,14 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var clickMeButton: NSButton!
+    
+    let MESSAGE = "Hello World!"
+    let INFORMATIVE_TEXT = "This is my first App in Swift"
+    let OK = "Ok"
+    let CANCEL = "Cancel"
+    let OK_MESSAGE = "You pressed OK"
+    let CANCEL_MESSAGE = "You pressed Cancel"
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -22,6 +29,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        let alert: NSAlert = NSAlert()
+        alert.messageText = MESSAGE
+        alert.informativeText = INFORMATIVE_TEXT
+        alert.alertStyle = NSAlert.Style.informational
+        alert.addButton(withTitle: OK)
+        alert.addButton(withTitle: CANCEL)
+        let res = alert.runModal()
+        let newButtonMessage = (res == NSApplication.ModalResponse.alertFirstButtonReturn ? OK_MESSAGE : CANCEL_MESSAGE)
+        clickMeButton.title = newButtonMessage
+    }
+    
 }
 
--- HelloWorldUITests/HelloWorldUITests.swift
+++ HelloWorldUITests/HelloWorldUITests.swift
@@ -9,21 +9,25 @@
 import XCTest
 
 class HelloWorldUITests: XCTestCase {
+   
 
+    
+    /// Put setup code here. This method is called before the invocation of each test method in the class.
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
+       
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
+    
+    /// Put teardown code here. This method is called after the invocation of each test method in the class.
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
     func testExample() {
@@ -31,4 +35,69 @@ class HelloWorldUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    
+    /// Test that the window is visible when the application starts
+    func testWindowOpens() {
+    /*
+        let app = XCUIApplication()
+        let helloworldWindow = app.windows["HelloWorld"]
+        helloworldWindow.click()
+        helloworldWindow.buttons["Click Me!"].click()
+        app.dialogs["alert"].buttons["Ok"].click()
+ */
+        
+        let app = XCUIApplication()
+        let helloworldWindow = app.windows["HelloWorld"]
+        XCTAssertTrue(helloworldWindow.isHittable)
+    }
+    
+    func testCanClickButton() {
+        let app = XCUIApplication()
+        let helloworldWindow = app.windows["HelloWorld"]
+        let clickMeButton = helloworldWindow.buttons["Click Me!"]
+        XCTAssertTrue(clickMeButton.isHittable)
+        clickMeButton.click()
+    }
+    
+    func testAlertWindowOpensAfterButtonClick() {
+        let app = XCUIApplication()
+        let helloworldWindow = app.windows["HelloWorld"]
+        let clickMeButton = helloworldWindow.buttons["Click Me!"]
+        clickMeButton.click()
+        let alertWindow = app.dialogs["alert"]
+        XCTAssertTrue(alertWindow.isHittable)
+    }
+    
+    func testOkAndCancelButtonOnAlertWindow() {
+        let app = XCUIApplication()
+        let helloworldWindow = app.windows["HelloWorld"]
+        let clickMeButton = helloworldWindow.buttons["Click Me!"]
+        clickMeButton.click()
+        let alertWindow = app.dialogs["alert"]
+        XCTAssertTrue(alertWindow.buttons["Ok"].isHittable)
+        XCTAssertTrue(alertWindow.buttons["Cancel"].isHittable)
+    }
+    
+    func testOkButtonClick() {
+        let app = XCUIApplication()
+        let helloworldWindow = app.windows["HelloWorld"]
+        helloworldWindow.click()
+        helloworldWindow.buttons["Click Me!"].click()
+        app.dialogs["alert"].buttons["Ok"].click()
+        XCTAssertTrue(helloworldWindow.buttons["You pressed OK"].isHittable)
+        print(helloworldWindow.buttons.firstMatch.debugDescription)
+        XCTAssertEqual(helloworldWindow.buttons.firstMatch.title, "You pressed OK")
+    }
+    
+    func testCancelButtonClick() {
+        let app = XCUIApplication()
+        let helloworldWindow = app.windows["HelloWorld"]
+        helloworldWindow.click()
+        helloworldWindow.buttons["Click Me!"].click()
+        app.dialogs["alert"].buttons["Cancel"].click()
+        XCTAssertTrue(helloworldWindow.buttons["You pressed Cancel"].isHittable)
+        print(helloworldWindow.buttons.firstMatch.debugDescription)
+        XCTAssertEqual(helloworldWindow.buttons.firstMatch.title, "You pressed Cancel")
+    }
+    
 }
